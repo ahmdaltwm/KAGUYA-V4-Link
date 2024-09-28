648591aabb139d1081e9b4db8155f120aae274eb
Jonell Hutchins Magallanes <jonellmagallanescc102005@gmail.com>
2024-09-28 11:02:26 +0000

    PORT UPDATED HSHS

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import express from "express";
 import config from "./KaguyaSetUp/config.js";
 import { log } from "./logger/index.js";
 var app = express();
-
+const PORT = process.env.PORT ||  config.port || 8040;
 app.get("/", (req, res) => {
   res.send("<h1>Kaguya Bot is running!</h1>");
 });
 
-app.listen(config.port, () => {
+app.listen(PORT, () => {
   log([
     {
       message: "[ EXPRESSJS ]: ",
