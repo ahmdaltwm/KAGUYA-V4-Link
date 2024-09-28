3dda6f650998cb78767d36fd0c04b55fe36e7284
Kaguya31Project <pvtdacayananarjhil@outlook.com>
2024-09-28 10:55:46 +0000

    .

--- Kaguya/Admin/changebio.js
+++ Kaguya/Admin/changebio.js
@@ -3,14 +3,19 @@ export default {
     author: "Arjhil Dacayanan",
     role: "owner",
     cooldowns: 10,
-    description: "Change bot's biography",
-    async execute({ api, args }) {
-      try {
-        var content = args.join(" ") || "";
-        await api.changeBio(content);
-        return kaguya.reply(`Bot's biography has been changed to: ${content}`)
-      } catch (err) {
-        console.error(err);
-      }
+    description: "Change bot's biography and edit a message",
+    async execute({ api, args, event }) {
+        try {
+            const content = args.join(" ") || "";
+
+            // Send initial message saying the bio is being updated
+            const initialMessage = await api.sendMessage("\n━━━━━━━━━━━━━━━━━━\nKaguya Updating Bio, Please Wait...\n━━━━━━━━━━━━━━━━━━\n", event.threadID);
+
+            // Update the initial message with the new bio info
+            const editedMessage = `Kaguya Biography updated to:\n━━━━━━━━━━━━━━━━━━\n ${content}\n━━━━━━━━━━━━━━━━━━\n`;
+            await api.editMessage(editedMessage, initialMessage.messageID);
+        } catch (err) {
+            console.error(err);
+        }
     },
 };
